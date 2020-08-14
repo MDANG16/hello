fbc1e0a7d72b1ac11efeaa1aef33844b4af7ea00
MDANG16 <mdang3@huskers.unl.edu>
2020-08-14 16:37:00 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+ /**
+ * Author: Minh Dang
+ *
+ * Date:   2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
